7e534860dbe2fc0302031d6fc89ff9632319a6cc
wangjunfeng123 <1159622457@qq.com>
2018-10-31 09:26:04 +0800

    动态代理

--- src/main/java/com/ninep/jubu/test/proxy/MyClassLoader.java
+++ src/main/java/com/ninep/jubu/test/proxy/MyClassLoader.java
@@ -1,5 +1,7 @@
 package com.ninep.jubu.test.proxy;
 
+import java.io.File;
+
 /**
  * @author wangjunfeng
  * @version 1.0
@@ -7,5 +9,19 @@ package com.ninep.jubu.test.proxy;
  * @since 2018/10/26
  */
 public class MyClassLoader extends ClassLoader{
+
+    private File classPathFile;
+
+    //去class文件生成的位置读取class文件
+    public MyClassLoader(){
+        String path = MyClassLoader.class.getResource("").getPath();
+        this.classPathFile = new File(path);
+    }
+
+    //加载class文件到jvm中
+    public Class<?> findClass(String name) {
+
+        return null;
+    }
 
 }
