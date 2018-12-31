09b929deddafc1f6b39812929be1fdfcdfb06bf6
wangjunfeng123 <wangjunfeng@kunchn.com>
2018-12-31 09:16:04 +0800

    rmi 执行步骤

--- src/main/java/com/ninep/jubu/test/rmi/RmiServer.java
+++ src/main/java/com/ninep/jubu/test/rmi/RmiServer.java
@@ -22,6 +22,12 @@ public class RmiServer {
     // 3.2 定义client端配置，用client端调用server端，看是否可以调通
 
 
+    // 执行步骤
+    // 1、server先注册服务，然后创建registry，registry再次创建studo
+    // 2、client端访问对应的服务registry，返回stu的对象
+    // 3、客户端远程调用skeleton，最后通过skeleton调用自定义的服务端接口，调用方法
+    // 4、获取返回结果
+
     public static void main(String[] args) {
         try {
             SayHello hello = new SayHelloImpl();
