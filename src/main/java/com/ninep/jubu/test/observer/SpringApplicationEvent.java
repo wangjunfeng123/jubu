0a3f00a496f1070139119c2261eb2fd5c47b4fd8
wangjunfeng123 <wangjunfeng@kunchn.com>
2019-08-28 17:14:29 +0800

    springboot event

---
+++ src/main/java/com/ninep/jubu/test/observer/SpringApplicationEvent.java
@@ -0,0 +1,42 @@
+package com.bigData.farm.config;
+
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * @author wangjunfeng
+ * @version 1.0
+ * @desc The type Sale menu service.
+ * @since 2019/8/28
+ */
+public class SpringApplicationEvent extends ApplicationEvent {
+    public SpringApplicationEvent(Object source) {
+        super(source);
+    }
+
+    public static void main(String[] args) {
+        // 注册监听器
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext();
+        context.addApplicationListener(applicationEvent ->
+                System.out.println(applicationEvent.getSource()));
+
+        context.refresh();
+
+        // 发布监听事件
+        context.publishEvent(new MyApplicationEvent("hello"));
+        context.publishEvent(new MyApplicationEvent("1"));
+        context.publishEvent(new MyApplicationEvent(new Integer(100)));
+
+    }
+
+    /**
+     * 监听事件
+     */
+    private static class MyApplicationEvent extends ApplicationEvent {
+        public MyApplicationEvent(Object source) {
+            super(source);
+        }
+    }
+
+}
