1ec1c99f72b82a3cf19edfffaf143f8d4b4efa80
wangjunfeng123 <wangjunfeng@kunchn.com>
2019-08-28 09:15:01 +0800

    observer model

---
+++ src/main/java/com/ninep/jubu/test/observer/ObserverDemo.java
@@ -0,0 +1,32 @@
+package com.ninep.jubu.test.observer;
+
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ * @author wangjunfeng
+ * @version 1.0
+ * @description 观察者模式
+ * @since 2019/8/28
+ */
+public class ObserverDemo {
+
+
+    public static void main(String[] args) {
+
+        MyObservable observable = new MyObservable();
+
+
+        observable.addObserver((o, value) -> System.out.println(value));
+
+        observable.setChanged();
+        observable.notifyObservers("Hello world");
+    }
+
+    public static class MyObservable extends Observable{
+        public void setChanged() {
+            super.setChanged();
+        }
+    }
+
+}
